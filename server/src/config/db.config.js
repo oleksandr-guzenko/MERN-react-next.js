9e8094b0b705445e71d89dda655cf16e08a5b07c
DerianCordobaPerez <derianunanleon@gmail.com>
2021-11-13 01:53:49 -0600

    Deploy heroku, create and connect mongodb cluster

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,5 +1,9 @@
-const DB_HOST = process.env.DB_HOST || 'localhost'
-const DB_PORT = process.env.DB_PORT || 27017
-const DB_NAME = process.env.DB_NAME || 'test'
+import dotenv from 'dotenv'
+dotenv.config()
 
-export const DB_URL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
+const DB_HOST = process.env.DB_HOST
+const DB_NAME = process.env.DB_NAME
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+
+export const DB_URL = `'mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority'`
