7f7fd9d159369fd418fd1a69ef45fce2239b923b
DragonWarrior980706 <dragon.warrior980706@gmail.com>
2021-11-17 00:11:23 -0600

    Protection with authentication middleware within movie paths and switch within authentication controllers

--- server/src/routes/movie.routes.js
+++ server/src/routes/movie.routes.js
@@ -5,8 +5,11 @@ import {Create, Find, Remove, Update} from '../controller/movie.controller'
 const router = Router()
 
 router.post('/movie/create', verifyToken, Create)
+
 router.get('/movie/find/:id?', verifyToken, Find)
+
 router.put('/movie/update/:id', verifyToken, Update)
+
 router.delete('/movie/remove', verifyToken, Remove)
 
 export default router
