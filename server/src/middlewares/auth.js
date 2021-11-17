9a6f3b904e4ec9c25322a7e3bde896a120f48db8
DragonWarrior980706 <dragon.warrior980706@gmail.com>
2021-11-16 23:27:08 -0600

    Modification within the authentication middleware

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -4,15 +4,16 @@ import User from '../models/user.model'
 export const verifyToken = (req, res, next) => {
   try {
     const token = req.headers['x-access-token']
+    
     if (!token)
       return res.status(401).send({
         message: 'No token provided.'
       })
 
     const decoded = jsonWebToken.verify(token, process.env.JWT_SECRET)
-    req.userId = decoded.id
+    req.user = decoded
 
-    const user = User.findById(req.userId, {password: 0})
+    const user = User.findById(req.user.id, {password: 0})
     if (!user)
       return res.status(404).send({
         message: 'User not found.'
