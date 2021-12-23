40d887d6bac74ddd7a86bea8cc19cd36d8681c4c
DragonWarrior980706 <dragon.warrior980706@gmail.com>
2021-12-23 17:39:12 -0600

    Promise managment

--- client/services/movies.js
+++ client/services/movies.js
@@ -2,14 +2,14 @@ import axios from 'axios'
 import {MOVIES_API_URL} from 'config/api.config'
 
 export const getMoviesService = async ({token}) => {
-  return axios.get(
+  return await axios.get(
     `${MOVIES_API_URL}/find`, {
       headers: {'x-auth-token': token}
     })
 }
 
 export const getMovieService = async ({token, id}) => {
-  return axios.get(
+  return await axios.get(
     `${MOVIES_API_URL}/find/${id}`, {
       headers: {'x-auth-token': token}
     })
